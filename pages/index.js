1b48d4465312462f3cffdd53722f407251511a9b
iEXCEL-UNMC <85242160+iEXCEL-UNMC@users.noreply.github.com>
2022-10-18 23:03:02 -0500

    cdsds

--- pages/index.js
+++ pages/index.js
@@ -29,16 +29,7 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
         <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-  <header class="mb-auto">
-    <div>
-      <h3 class="float-md-start mb-0">Cover</h3>
-      <nav class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
-        <a class="nav-link fw-bold py-1 px-0" href="#">Features</a>
-        <a class="nav-link fw-bold py-1 px-0" href="#">Contact</a>
-      </nav>
-    </div>
-  </header>
+  
 
   <main class="px-3">
   <div class="py-3 pb-5 text-center text-white vh-110 overflow-x-hidden align-items-center d-flex" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, .75), rgba(0, 0, 0, .80)), url(https://res.cloudinary.com/nkh/image/upload/v1665634989/bio.jpg);  background-position: center center, center center;  background-size: cover, cover;  background-repeat: no-repeat;">
