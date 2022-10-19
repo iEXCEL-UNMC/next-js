d9433c16dfd5f7a5932a16232397856e2fdf3e82
iEXCEL-UNMC <85242160+iEXCEL-UNMC@users.noreply.github.com>
2022-10-18 21:49:33 -0500

    hhhh

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,22 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Neil Humphrey UX designer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
+      <div class="container">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      </ul>
+    </header>
+  </div>
         <Header title="NEXT.JS Blank Template - iEXCEL at UNMC" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
