cd4a50cc50dca772dc0ec43f6280db2171c8da44
iEXCEL-UNMC <85242160+iEXCEL-UNMC@users.noreply.github.com>
2022-10-18 23:04:46 -0500

    Revert "sdsds"
    This reverts commit b6d33dcdb1a978b1be31ecfb3925a4e514850005.

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,10 @@ export default function Home() {
       <Head>
         <title>Neil Humphrey UX designer</title>
         <link rel="icon" href="/favicon.ico" />
-      
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></link>
       </Head>
 
       <main>
-      <div class="container-xxl">
+      <div class="container">
     <header class="d-flex justify-content-center py-3">
       <ul class="nav nav-pills">
         <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
