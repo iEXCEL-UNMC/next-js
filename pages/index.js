4018675aaa45d7e5019398e5dc7bc074655c3190
iEXCEL-UNMC <85242160+iEXCEL-UNMC@users.noreply.github.com>
2022-10-18 22:52:51 -0500

    sdsds

--- pages/index.js
+++ pages/index.js
@@ -41,11 +41,32 @@ export default function Home() {
   </header>
 
   <main class="px-3">
-    <h1>Cover your page.</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-    <p class="lead">
-      <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
-    </p>
+  <div class="py-3 pb-5 text-center text-white vh-110 overflow-x-hidden align-items-center d-flex" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, .75), rgba(0, 0, 0, .80)), url(https://res.cloudinary.com/nkh/image/upload/v1665634989/bio.jpg);  background-position: center center, center center;  background-size: cover, cover;  background-repeat: no-repeat;">
+    <div class="container py-3">
+      <div class="row  w-1 mx-auto px-4 ">
+        <div class="mx-auto col-lg-7 col-12 gy d-flex flex-column justify-content-evenly p-3" style="
+    /* max-width: 48em; */
+">
+          <h1 class="  display-3 mb-3 lh-sm text-white mt-0"><span class="dsds fw-bol" style="
+    text-shadow: 0 0 10px #111;
+    font-weight: 500;
+">People want to do great things.</span></h1><h2 class="fs-1 fw-l mb-4 text-hite" style="
+    text-shadow: 1px 1px 1px #0b0b0b;
+    font-weight: 400;
+    color: #8dd6ac !important;
+    letter-spacing: 0px;
+"> Help them get there.</h2>
+          <p class="  lh-base p-2" style="
+    /* max-width: 47vw; */
+    margin: auto;
+    text-shadow: 1px 1px 1px black;
+    font-weight: 300;
+    line-height: 1.5!important;
+font-size:clamp(1.2rem, -0.5vw + 1.3rem, 1rem)!important;">Founded in 2016 by Silicon Valley people leader Beth Steinberg, Mensch Ventures is a People and Talent advisory firm focused on helping companies scale, enhance culture and drive business results.</p> 
+        </div>
+      </div>
+    </div>
+  </div>
   </main>
 </div>
       </main>
